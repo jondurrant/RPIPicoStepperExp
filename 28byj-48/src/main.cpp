2c2d036611c9e90a28d109311ff1a386b1402afa
John Lienau <jlienau@eyefactive.com>
2023-05-12 02:29:58 +0200

    Improved indenstations

--- 28byj-48/src/main.cpp
+++ 28byj-48/src/main.cpp
@@ -5,63 +5,53 @@
  */
 
 #include "pico/stdlib.h"
-
-#define DELAY 2
-
-#define LED 0
-
 #include <stdio.h>
 
+#define DELAY 2
+#define LED 0
 
+int main()
+{
+  bool on = false;
 
-int main() {
+  uint8_t stepPads[4] = {2, 3, 4, 5};
+  uint8_t sequence[4] = {3, 6, 12, 9};
+  uint8_t halfSeq[8] = {3, 7, 6, 15, 12, 13, 9, 11};
+  uint8_t s;
 
-	bool on = false;
+  stdio_init_all();
 
-	uint8_t stepPads[4] = {2,3,4,5};
+  sleep_ms(2000);
+  printf("GO\n");
 
-	uint8_t sequence[4] = {3, 6, 12, 9};
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+  gpio_put(LED, on);
 
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    gpio_init(stepPads[i]);
+    gpio_set_dir(stepPads[i], GPIO_OUT);
+    gpio_put(stepPads[i], 0);
+  }
 
-	uint8_t halfSeq[8] = {3, 7, 6, 15, 12, 13, 9, 11  };
+  while (true)
+  {
+    for (uint8_t i = 0; i < 4; i++)
+    {
+      s = sequence[i];
+      for (uint8_t b = 0; b < 4; b++)
+      {
+        uint8_t m = 1 << b;
+        if ((m & s) > 0)
+          gpio_put(stepPads[b], 1);
+        else
+          gpio_put(stepPads[b], 0);
+      }
+      sleep_ms(DELAY);
+    }
 
-	uint8_t s;
-
-	stdio_init_all();
-
-	sleep_ms(2000);
-	printf("GO\n");
-
-	gpio_init(LED);
-	gpio_set_dir(LED, GPIO_OUT);
-	gpio_put(LED, on);
-
-	for (uint8_t i=0; i < 4; i++){
-		gpio_init(stepPads[i]);
-		gpio_set_dir(stepPads[i], GPIO_OUT);
-		gpio_put(stepPads[i], 0);
-	}
-
-
-	for(;;) {
-		for (uint8_t i=0; i < 4; i++){
-
-			s = sequence[i];
-			for (uint8_t b = 0; b < 4; b++){
-				uint8_t m = 1 << b;
-				if ((m & s) > 0){
-					gpio_put(stepPads[b], 1);
-				} else {
-					gpio_put(stepPads[b], 0);
-				}
-
-			}
-			sleep_ms(DELAY);
-
-		}
-
-		on = ! on;
-		gpio_put(LED, on);
-	}
-
+    on = !on;
+    gpio_put(LED, on);
+  }
 }
